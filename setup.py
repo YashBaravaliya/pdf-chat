39a9e9171798b409dcd94c4b2b2633061d4cf3bf
Yash Baravaliya <yashlg567@gmail.com>
2024-03-24 15:35:10 +0530

    adding src

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'rag',
+    version= '0.0.0',
+    author= 'yash',
+    author_email= 'yashbaravaliya206@gmail.com',
+    packages= find_packages(),
+    install_requires = []
+
+)
---
+++ src/helper.py
@@ -0,0 +1,9 @@
+template="""Use the following pieces of information to answer the user's question.
+If you dont know the answer just say you know, don't try to make up an answer.
+
+Context:{context}
+Question:{question}
+
+Only return the helpful answer below and nothing else. it is stricly restriget you ansewer the question other then the my information even i ask you to answer the question.
+"""
+
